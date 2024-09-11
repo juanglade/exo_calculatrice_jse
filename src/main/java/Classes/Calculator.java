7520cafd30416f902f68cdc37d07f695bbe57399
Anglade <julienangladedev@gmail.com>
2024-09-11 21:44:42 +0200

    Après migration du code dans la classe calculator

--- src/main/java/Classes/Calculator.java
+++ src/main/java/Classes/Calculator.java
@@ -1,6 +1,16 @@
 package Classes;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -11,11 +21,69 @@ public class Calculator extends JFrame {
     public Calculator() {
         //configurer le comportement de la fenêtre
         setupGui();   //Generateur Utilisateur Interface
-        this.pack();
-        this.setVisible(true);
+
+        //Créer et configurer le JFrame
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //Récupérer le contentPane
+        Container contentPane = f.getContentPane();
+        //Ajouter le BorderLayout()
+        contentPane.setLayout(new BorderLayout(5, 3));
+
+        //Créer un label écran pour affichage des résultats
+        //double resultat = 0.0;
+        JLabel ecran = new JLabel("0", JLabel.RIGHT);
+        ecran.setFont(new Font("Arial", Font.PLAIN, 24));
+        ecran.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        ecran.setOpaque(true);
+        ecran.setBackground(Color.WHITE);
+        ecran.setPreferredSize(new Dimension(300, 50));
+
+        //Créer les 2 JPanels pour les boutons chiffres et opérations
+        JPanel panelDeGauche = new JPanel();
+        JPanel panelDeDroite = new JPanel();
+
+        //Ajouter le JLabel au nord
+        contentPane.add(ecran, BorderLayout.NORTH);
+
+        //Ajouter les JPanels aux zones west et east
+        contentPane.add(panelDeGauche, BorderLayout.WEST);
+        contentPane.add(panelDeDroite, BorderLayout.EAST);
+
+        // Créer les Jboutons de gauche et les ajouter au JPanel de gauche
+        String[] boutonsDeGauche = {"7", "8", "9", "4", "5", "6", "3", "2", "1", "0", ".", "="};
+        for (String texte : boutonsDeGauche) {
+            JButton bouton = new JButton(texte);
+            bouton.setFont(new Font("Arial", Font.PLAIN, 20));
+            panelDeGauche.add(bouton);
+        }
+
+        // Créer les Jboutons de droite et les ajouter au JPanel de droite
+        String[] boutonsDeDroite = {"CE", "C", "+", "-", "X", "/"};
+        for (String texte : boutonsDeDroite) {
+            JButton bouton = new JButton(texte);
+            bouton.setFont(new Font("Arial", Font.PLAIN, 20));
+            panelDeDroite.add(bouton);
+        }
+
+        //Panel pour les boutons de gauche
+        panelDeGauche.setBounds(50, 100, 300, 300);
+        panelDeGauche.setLayout(new GridLayout(4, 3));
+
+        //Panel pour les boutons de droite
+        panelDeDroite.setBounds(50, 100, 300, 300);
+        panelDeDroite.setLayout(new GridLayout(3, 2));
+
+        setupGui();   //Generateur Utilisateur Interface
+
+        //Compacter les élements du panel
+        f.pack();
+        // Afficher le panel
+        f.setVisible(true);
     }
 
     private void setupGui() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         //création contenu fenêtre
     }
 }
--- src/main/java/com/mycompany/exo_calculatrice_ja/Exo_calculatrice_ja.java
+++ src/main/java/com/mycompany/exo_calculatrice_ja/Exo_calculatrice_ja.java
@@ -3,17 +3,7 @@
  */
 package com.mycompany.exo_calculatrice_ja;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import Classes.Calculator;
 
 /**
  *
@@ -22,61 +12,8 @@ import javax.swing.JPanel;
 public class Exo_calculatrice_ja {
 
     public static void main(String[] args) {
-        //Créer et configurer le JFrame
-        JFrame f = new JFrame();
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //Récupérer le contentPane
-        Container contentPane = f.getContentPane();
-        //Ajouter le BorderLayout()
-        contentPane.setLayout(new BorderLayout(5, 3));
-
-        //Créer un label écran pour affichage des résultats
-        //double resultat = 0.0;
-        JLabel ecran = new JLabel("0", JLabel.RIGHT);
-        ecran.setFont(new Font("Arial", Font.PLAIN, 24));
-        ecran.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        ecran.setOpaque(true);
-        ecran.setBackground(Color.WHITE);
-        ecran.setPreferredSize(new Dimension(300, 50));
-
-        //Créer les 2 JPanels pour les boutons chiffres et opérations
-        JPanel panelDeGauche = new JPanel();
-        JPanel panelDeDroite = new JPanel();
-
-        //Ajouter le JLabel au nord
-        contentPane.add(ecran, BorderLayout.NORTH);
-
-        //Ajouter les JPanels aux zones west et east
-        contentPane.add(panelDeGauche, BorderLayout.WEST);
-        contentPane.add(panelDeDroite, BorderLayout.EAST);
-
-        // Créer les Jboutons de gauche et les ajouter au JPanel de gauche
-        String[] boutonsDeGauche = {"7", "8", "9", "4", "5", "6", "3", "2", "1", "0", ".", "="};
-        for (String texte : boutonsDeGauche) {
-            JButton bouton = new JButton(texte);
-            bouton.setFont(new Font("Arial", Font.PLAIN, 20));
-            panelDeGauche.add(bouton);
-        }
-
-        // Créer les Jboutons de droite et les ajouter au JPanel de droite
-        String[] boutonsDeDroite = {"CE", "C", "+", "-", "X", "/"};
-        for (String texte : boutonsDeDroite) {
-            JButton bouton = new JButton(texte);
-            bouton.setFont(new Font("Arial", Font.PLAIN, 20));
-            panelDeDroite.add(bouton);
-        }
-
-        //Panel pour les boutons de gauche
-        panelDeGauche.setBounds(50, 100, 300, 300);
-        panelDeGauche.setLayout(new GridLayout(4, 3));
-
-        //Panel pour les boutons de droite
-        panelDeDroite.setBounds(50, 100, 300, 300);
-        panelDeDroite.setLayout(new GridLayout(3, 2));
-
-        //Compacter les élements du panel
-        f.pack();
-        // Afficher le panel
-        f.setVisible(true);
+        //instance de calculator
+        Calculator calculator = new Calculator();
     }
+
 }
